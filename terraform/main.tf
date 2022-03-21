38f218348cd015d86e3ec61a573fe2c70210285a
sainathrudra7 <97375734+sainathrudra7@users.noreply.github.com>
2022-03-21 20:18:15 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-    region = "us-east-1"
-    aws_access_key_id = "AKIAXMZR7CJW6IJL73XF"
-    aws_secret_access_key = "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"
+    region = "ami-09662e4f2b2fb67f9"
+    aws_access_key_id = "AKIAQR4BCNOGONR5RTJ3"
+    aws_secret_access_key = "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"
 }
 
 
 resource "aws_instance" "sainath" {
     ami =  "ami-0e472ba40eb589f49"
     instance_type = "t2.micro"
-    key_name = "rrr"
+    key_name = "moji"
 
     tags = {
             Name = "myinstance"
